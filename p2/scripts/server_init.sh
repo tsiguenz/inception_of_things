ff98ec0eb380e89f963a9dfaed321e966ea71226
tsiguenz <43640980+tsiguenz@users.noreply.github.com>
2023-12-01 02:35:14 +0100

    p2 confs/service.yaml looks ok

--- p2/scripts/server_init.sh
+++ p2/scripts/server_init.sh
@@ -7,15 +7,10 @@ echo "Installing k3s..."
 SERVER_IP="192.168.56.110"
 FLAGS="--node-ip $SERVER_IP"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=$FLAGS sh -
-chmod 644 /etc/rancher/k3s/k3s.yaml
-
-echo "Installing helm3..."
-
-# Yes I'm an acrobat (https://helm.sh/docs/intro/install/#from-script)
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sh
+# chmod 644 /etc/rancher/k3s/k3s.yaml
 
 echo "Deploying apps..."
 
-# sudo kubectl apply -f deployment.yaml
-# sudo kubectl apply -f service.yaml
-# sudo kubectl apply -f ingress.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
