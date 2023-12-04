eb52a41b1645a623d2afbeedb15f900e5ae5b9ab
tsiguenz <43640980+tsiguenz@users.noreply.github.com>
2023-12-04 18:26:18 +0100

    fix confs files path and refacto

--- p2/scripts/server_init.sh
+++ p2/scripts/server_init.sh
@@ -6,13 +6,13 @@ echo "Installing k3s..."
 
 SERVER_IP="192.168.56.110"
 FLAGS="--node-ip $SERVER_IP"
+CONFS_PATH="/vagrant/confs"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=$FLAGS sh -
-# chmod 644 /etc/rancher/k3s/k3s.yaml
 
 echo "Deploying apps..."
 
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
-kubectl apply -f ingress.yaml
+kubectl apply -f "$CONFS_PATH"/deployment.yaml
+kubectl apply -f "$CONFS_PATH"/service.yaml
+kubectl apply -f "$CONFS_PATH"/ingress.yaml
 
 chmod 644 /etc/rancher/k3s/k3s.yaml
